b6d68880746eebb966dbf7a5f0e490f8244a3db8
Merola Giuseppe Pierpaolo <sennar.pierp@gmail.com>
2024-06-20 11:47:50 +0100

    Add files via upload

---
+++ pairwise alignment/include/alignment.h
@@ -0,0 +1,8 @@
+#ifndef ALIGNMENT_H
+#define ALIGNMENT_H
+
+#include <string>
+
+std::pair<std::string, std::string> pairwise_alignment(const std::string& seq1, const std::string& seq2);
+
+#endif // ALIGNMENT_H
